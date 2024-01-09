89f94c4424999b38297874d6b1f83bffc63b035c
ayber <ayber@AYBERKYAMAN>
2024-01-09 12:58:23 +0300

    Add project files.

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Drawing;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string message)
+        {
+
+            await Clients.All.SendAsync("YeniMesaj", name, message);
+
+        }
+        public async Task SendLocation(string color, int x, int y)
+        {
+            await Clients.All.SendAsync("GetLocation",Context.ConnectionId, color, x, y);
+        }
+    }
+}
